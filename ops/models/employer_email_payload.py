42ccba0c0e94756373637694bea5b6861615103c
prachir1501 <prachir1501@gmail.com>
2023-09-15 14:28:30 +0530

    add : date to employer email payload

--- ops/models/employer_email_payload.py
+++ ops/models/employer_email_payload.py
@@ -3,7 +3,16 @@ from pydantic import BaseModel, Field
 # add date field as well , get from async-jobs
 
 
+class RequestDate(BaseModel):
+
+    # required fields
+    year: int
+    month: int
+    day: int
+
+
 class EmployerEmailPayload(BaseModel):
 
     # required fields
     employer_id: str = Field(alias="employerId")
+    request_date: RequestDate = Field(alias="requestDate")
