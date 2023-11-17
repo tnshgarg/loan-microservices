6ca3cf92a54b022c29727e6cb90957e170285cdc
prachir1501 <prachir1501@gmail.com>
2023-11-17 17:23:07 +0530

    add : sort, search, filter

--- admin/views/employer_leads_view.py
+++ admin/views/employer_leads_view.py
@@ -8,16 +8,50 @@ from starlette_admin import BaseModelView, StringField
 from admin.services.bureau_fetch_service import BureauFetchService
 from admin.utils import DictToObj
 from dal.models.employer_leads import EmployerLeads
+from dal.models.sales_users import SalesUser
 
 
-def trigger_bureau_fetch(inserted_employer_lead_object):
-    name = inserted_employer_lead_object.name
-    mobile = inserted_employer_lead_object.mobile
-    pan = inserted_employer_lead_object.pan
+def trigger_bureau_fetch(inserted_object):
+    name = inserted_object.name
+    mobile = inserted_object.mobile
+    pan = inserted_object.pan
     try:
         BureauFetchService().fetch_bureau_details(name, mobile, pan)
     except Exception as e:
         raise HTTPException(500, str(e))
+
+
+def create_search_filter(sales_user, term):
+    filter_ = {}
+    if sales_user["roles"] in [SalesUser.Type.RM, SalesUser.Type.SM]:
+        filter_["sales_id"] = bson.ObjectId(sales_user["sales_id"])
+
+    if term is None or not isinstance(term, (str, int)):
+        return filter_
+
+    expressions = []
+    for field in EmployerLeadsView.fields:
+        if not field.exclude_from_list:
+            expressions.append(
+                {
+                    field.name: {
+                        "$regex": term,
+                        "$options": "i"
+                    }
+                }
+            )
+    filter_["$or"] = expressions
+    return filter_
+
+
+def create_sorter(order_by):
+    sorter = []
+    if order_by is not None:
+        for field_info in order_by:
+            field, order = field_info.split(maxsplit=1)
+            direction = 1 if order == "asc" else -1
+            sorter.append((field, direction))
+    return sorter
 
 
 class EmployerLeadsView(BaseModelView):
@@ -27,7 +61,7 @@ class EmployerLeadsView(BaseModelView):
     icon = "fa fa-id-card"
     pk_attr = "_id"
     fields = [
-        StringField("_id"),
+        StringField("_id", exclude_from_list=True),
         StringField("name"),
         StringField("mobile"),
         StringField("pan")
@@ -37,57 +71,44 @@ class EmployerLeadsView(BaseModelView):
         model = EmployerLeads
 
     async def count(self, request: Request, where: Union[Dict[str, Any], str, None] = None) -> int:
-        filter_ = {}
-        # if where is not None:
-        #     filter_ = where
-        employer_leads_res = self.Meta.model.find(filter_)
-        return len(list(employer_leads_res))
+        filter_ = create_search_filter(request.state.user, where)
+        find_res = self.Meta.model.find(filter_)
+        return len(list(find_res))
 
     async def find_all(self, request: Request, skip: int = 0, limit: int = 100,
                        where: Union[Dict[str, Any], str, None] = None,
                        order_by: Optional[List[str]] = None) -> List[Any]:
-        employer_leads_res = self.Meta.model.find({})
-        employer_leads_res.skip(skip).limit(limit)
-        order_by
-        find_all_res = []
-        for employer_lead in employer_leads_res:
-            find_all_res.append(DictToObj(employer_lead))
-        return find_all_res
-        # values = list(db.values())
-        # if order_by is not None:
-        #     assert len(order_by) < 2, "Not supported"
-        #     if len(order_by) == 1:
-        #         key, dir = order_by[0].split(maxsplit=1)
-        #         values.sort(key=lambda v: getattr(
-        #             v, key), reverse=(dir == "desc"))
-
-        # if where is not None and isinstance(where, (str, int)):
-        #     values = filter_values(values, where)
-        # if limit > 0:
-        #     return values[skip: skip + limit]
-        # return values[skip:]
+        filter_ = create_search_filter(request.state.user, where)
+        sorter = create_sorter(order_by)
+        find_res = self.Meta.model.find(filter_).sort(
+            sorter).skip(skip).limit(limit)
+        find_res_objects = []
+        for doc in find_res:
+            find_res_objects.append(DictToObj(doc))
+        return find_res_objects
 
     async def find_by_pk(self, request: Request, pk):
-        employer_leads_res = EmployerLeads.find_one({"_id": bson.ObjectId(pk)})
-        return DictToObj(employer_leads_res)
+        find_one_res = self.Meta.model.find_one(
+            {"_id": bson.ObjectId(pk)})
+        return DictToObj(find_one_res)
 
     async def create(self, request: Request, data: Dict):
         sales_id = request.state.user.get("sales_id")
         data["sales_id"] = bson.ObjectId(sales_id)
 
-        employer_leads_insert_res = EmployerLeads.insert_one(data)
-        inserted_id = employer_leads_insert_res.inserted_id
+        insert_res = self.Meta.model.insert_one(data)
+        inserted_id = insert_res.inserted_id
         data["_id"] = inserted_id
-        inserted_employer_lead_object = DictToObj(data)
+        inserted_object = DictToObj(data)
 
-        await self.after_create(request, inserted_employer_lead_object)
-        return inserted_employer_lead_object
+        await self.after_create(request, inserted_object)
+        return inserted_object
 
     async def after_create(self, request: Request, obj: Any) -> None:
         trigger_bureau_fetch(obj)
 
     async def edit(self, request: Request, pk, data: Dict):
-        employer_leads_update_res = EmployerLeads.update_one(
+        update_res = self.Meta.model.update_one(
             filter_={"_id": bson.ObjectId(pk)},
             update={"$set": data}
         )
@@ -96,10 +117,10 @@ class EmployerLeadsView(BaseModelView):
 
     async def delete(self, request: Request, pks: List[Any]) -> Optional[int]:
         object_ids_to_delete = [bson.ObjectId(pk) for pk in pks]
-        employer_leads_delete_res = EmployerLeads.delete({
+        delete_res = self.Meta.model.delete({
             "_id": {
                 "$in": object_ids_to_delete
             }
         })
-        deleted_document_count = employer_leads_delete_res.deleted_count
+        deleted_document_count = delete_res.deleted_count
         return deleted_document_count
