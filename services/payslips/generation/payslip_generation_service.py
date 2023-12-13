b3d7c83791991b51b6f244ed434b289ca1093e15
Tanish garg <tanishg566@outlook.com>
2023-12-13 13:45:54 +0530

    mod: cms update and payslip rupee symbol fix

--- services/payslips/generation/payslip_generation_service.py
+++ services/payslips/generation/payslip_generation_service.py
@@ -4,7 +4,8 @@ import os
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from PyPDF2 import PdfWriter, PdfReader
-
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 from services.payslips.uploads.payslip_upload_service import PayslipUploadService
 
 from payslips.utils.convert_to_inr import convert_to_inr
@@ -14,6 +15,8 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.join(SCRIPT_DIR, '..', '..', '..')
 TEMPLATE_PATH = os.path.join(
     PARENT_DIR, "templates/pdf/unipe_payslip_template.pdf")
+RUPEE_SYMBOL = b'\xe2\x82\xb9'.decode()
+FONT_NAME = "Roboto"
 
 
 class PayslipGenerationService:
@@ -32,17 +35,17 @@ class PayslipGenerationService:
             header_date_str.replace("Z", "+00:00"))
         header_date = header_datetime.strftime("%b %Y").upper()
 
-        can.setFont("Helvetica", 16)
+        can.setFont(FONT_NAME, 16)
         can.drawString(36, 768, header_date)
-        can.setFont("Helvetica", 8)
+        can.setFont(FONT_NAME, 8)
         can.drawString(36, 750, header.get("company_name", "N/A"))
         can.drawString(36, 735, header.get("company_address", "N/A"))
 
     def _create_employee_details(self, can):
         employee = self.employee
-        can.setFont("Helvetica", 9)
+        can.setFont(FONT_NAME, 9)
         can.drawString(37, 695, employee.get("employee_name", "N/A"))
-        can.setFont("Helvetica", 8)
+        can.setFont(FONT_NAME, 8)
         can.drawString(36, 660, employee.get("employee_no", "N/A"))
         can.drawString(166, 660, employee.get("date_joined", "N/A"))
         can.drawString(297, 660, employee.get("department", "N/A"))
@@ -60,30 +63,41 @@ class PayslipGenerationService:
 
     def _create_earning_details(self, can):
         earnings = self.earnings
-        can.drawString(258, 480, earnings.get("basic", "N/A"))
-        can.drawString(258, 465, earnings.get("hra", "N/A"))
-        can.drawString(258, 450, earnings.get("other_allowance", "N/A"))
-        can.drawString(258, 415, earnings.get("total_earnings", "N/A"))
+        can.drawString(
+            258, 480, RUPEE_SYMBOL + earnings.get('basic', 'N/A'))
+        can.drawString(258, 465, RUPEE_SYMBOL + earnings.get("hra", "N/A"))
+        can.drawString(258, 450, RUPEE_SYMBOL +
+                       earnings.get("other_allowance", "N/A"))
+        can.drawString(258, 415, RUPEE_SYMBOL +
+                       earnings.get("total_earnings", "N/A"))
 
     def _create_deduction_details(self, can):
         deductions = self.deductions
-        can.drawString(520, 480, deductions.get("tax_deducted", "N/A"))
-        can.drawString(520, 465, deductions.get("pf_contribution", "N/A"))
-        can.drawString(520, 450, deductions.get("professional_tax", "N/A"))
-        can.drawString(520, 435, deductions.get("other_deductions", "N/A"))
-        can.drawString(520, 416, deductions.get("total_deductions", "N/A"))
+        can.drawString(520, 480, RUPEE_SYMBOL +
+                       deductions.get("tax_deducted", "N/A"))
+        can.drawString(520, 465, RUPEE_SYMBOL +
+                       deductions.get("pf_contribution", "N/A"))
+        can.drawString(520, 450, RUPEE_SYMBOL +
+                       deductions.get("professional_tax", "N/A"))
+        can.drawString(520, 435, RUPEE_SYMBOL +
+                       deductions.get("other_deductions", "N/A"))
+        can.drawString(520, 416, RUPEE_SYMBOL +
+                       deductions.get("total_deductions", "N/A"))
 
     def _create_final_details(self, can):
         final = self.final
         net_pay = convert_to_inr(final.get('net_pay', 'N/A'))
-        net_salary_in_words = convert_to_words(int(final.get("net_pay")[1:]))
-        can.drawString(258, 368, f"{net_pay}")
-        can.drawString(258, 353, f"{net_salary_in_words}")
+        net_salary_in_words = convert_to_words(int(final.get("net_pay")))
+        can.drawString(258, 368, RUPEE_SYMBOL + net_pay)
+        can.drawString(258, 353, net_salary_in_words)
 
     def _create_payslip_overlay(self):
         packet = io.BytesIO()
+        pdfmetrics.registerFont(
+            TTFont('Roboto', 'resources/Roboto-Regular.ttf'))
         can = canvas.Canvas(packet, pagesize=letter)
-        can.setFont("Helvetica", 12)
+
+        can.setFont(FONT_NAME, 12)
 
         self._create_header(can)
         self._create_employee_details(can)
--- services/payslips/uploads/payslip_upload_service.py
+++ services/payslips/uploads/payslip_upload_service.py
@@ -10,20 +10,27 @@ PAYSLIP_S3_BASE_PATH = "payslip_upload_service"
 class PayslipUploadService(MediaUploadService):
     def __init__(self,
                  employment_id: bson.ObjectId,
+                 employer_id: str,
+                 year: str, month: str
                  ) -> None:
         super().__init__(
             None,
             employment_id,
-            payslip_gdrive_upload_service(),
+            payslip_gdrive_upload_service(employer_id),
             payslip_s3_upload_service(),
-            payslip_google_sheets_service()
+            payslip_google_sheets_service(),
+
         )
+        self.employer_id = employer_id
         self.employment_id = employment_id
+        self.year = year
+        self.month = month
+        self.year_month = "{:d}-{:02d}".format(self.year, self.month)
 
     def _upload_media(self, fd, filename, extension, mime):
         idx_filename = f"{self.ts_prefix}_{filename}.{extension}"
         drive_upload_response = self.gdrive_upload_service.upload_file(
-            child_folder_name=str(self.employment_id),
+            child_folder_name=self.year_month,
             name=idx_filename,
             mime_type=mime,
             fd=fd,
@@ -31,7 +38,7 @@ class PayslipUploadService(MediaUploadService):
         )
 
         # TODO: Need to create a bucket for Payslips
-        s3_key = f"{PAYSLIP_S3_BASE_PATH}/{self.employment_id}/{idx_filename}"
+        s3_key = f"{self.employer_id}/{self.year_month}/{idx_filename}"
         status, _ = self.s3_upload_service.upload(
             key=s3_key,
             fd=fd,
@@ -51,7 +58,7 @@ class PayslipUploadService(MediaUploadService):
         fd.seek(0)
         drive_link, s3_key = self._upload_media(
             fd,
-            filename=f"{self.employment_id}{payslip_data['header']['date']}",
+            filename=f"{self.employment_id}",
             extension="pdf",
             mime="application/pdf"
         )
@@ -61,8 +68,8 @@ class PayslipUploadService(MediaUploadService):
         [drive_url, s3_key] = self._upload_payslip(
             fd, payslip_data
         )
-        self._add_to_db(drive_url, s3_key)
-        return drive_url
+        asset_url = self._add_to_db(drive_url, s3_key)
+        return asset_url
 
     def update_payslips(self, update):
         return Payslips.update_one(
@@ -73,7 +80,10 @@ class PayslipUploadService(MediaUploadService):
         )
 
     def _add_to_db(self, drive_url, s3_path):
+        asset_url = "https://d2ittjdu112xu6.cloudfront.net/" + s3_path
         self.update_payslips({
             f"documents.drive": drive_url,
             f"documents.s3": s3_path,
+            f"documents.url": asset_url
         })
+        return asset_url
